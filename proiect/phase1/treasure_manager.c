a4ff39a7513de5aa7d52695b3750cf163ea55676
AndreeaSabina2311 <andreeasabina2311@gmail.com>
2025-04-07 13:11:59 +0300

    Add files via upload

--- proiect/phase1/treasure_manager.c
+++ proiect/phase1/treasure_manager.c
@@ -264,7 +264,111 @@ int main(int argc, char **argv)
 	  exit(-1);
 	} 
     }
-    
-   
+  //remove_treasure: va sterge o comoara dintr-un fisier
+  //se va cauta vanatoare dupa argv[1], iar apoi se va cauta prin fisiere comoara de sters
+  //se va cauta ca la view. cand am gasit-o, o elimin
+  else if(strcmp(argv[1],"remove_treasure")==0)
+    {
+      DIR *dir;
+      snprintf(path, sizeof(path),"./%s",argv[2]);
+
+      if((dir=opendir(path))==NULL)
+	{
+	  perror("Nu s-a putut deschide directorul");
+	  exit(-1);
+	}
+      int id_arg=atoi(argv[3]);
+      int continua=1; //continui sa parcurg directorul vanatorii pana cand am gasit comoara; dupa ce am gasit-o, m-am oprit
+      while((entry=readdir(dir))!=NULL && continua==1)
+	{
+	  if(strstr(entry->d_name,".dat")!=NULL)
+	    {
+	      char filepath[512];
+	      snprintf(filepath,sizeof(filepath),"%s/%s",path,entry->d_name);
+
+	      int f;
+	      f=open(filepath,O_RDONLY);
+	      if(f==-1)
+		{
+		  perror("Eroare deschidere fisier din directorul dat ca argument");
+		  continue;
+		}
+
+	      //creem fisierul temporar
+	      char temp_filepath[1024];
+	      snprintf(temp_filepath,sizeof(temp_filepath),"%s/temp_%s",path,entry->d_name);
+	      int temp_f;
+	      temp_f=open(temp_filepath,O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR| S_IWUSR);
+	      if(temp_f==-1)
+		{
+		  perror("Eroare deshidere fisier temporar pentru a scrie(copia) fisierul original in el");
+		  close(f);
+		  continue;
+		}
+	      
+	      Treasure x;
+	      while(read(f,&x,sizeof(Treasure))==sizeof(Treasure))
+		{
+		  if(x.treasure_id==id_arg)
+		    {
+		      continua=0;
+		      continue; //nu copiem comoara pe care vrem sa o stergem
+		    }
+		  write(temp_f, &x, sizeof(Treasure));
+		}
+	      close(f);
+	      close(temp_f);
+
+	      if(continua==0)
+		{
+		  //inlocuim fisierul original cu cel temporar
+		  if(remove(filepath)==-1)
+		    {
+		      perror("Eroare stergere fisier original");
+		      exit(-1);
+		    }
+		  if(rename(temp_filepath, filepath)==-1)
+		    {
+		      perror("Eroare redenumire fisier temporar");
+		      exit(-1);
+		    }
+		  //logam actiunea
+		  char log_entry[512];
+		  int len=snprintf(log_entry, sizeof(log_entry),"Treasure %s from hunt %s was removed\n",argv[3],argv[2]);
+		  if(write(log_fd, log_entry, len) ==-1)
+		    {
+		      perror("eroare la scriere in log");
+		      close(log_fd);
+		      exit(-1);
+		    }
+		  if(close(log_fd)==-1)
+		    {
+		      perror("eroare inchidere fisier de log");
+		      exit(-1);
+		    } 
+		}
+	      else //daca nu s-a gasit comoara, stergem fisierul temporar
+		{
+		  remove(temp_filepath);
+		}
+
+	  
+	    }
+	     
+	}
+      closedir(dir);
+      if(continua==1)
+	{
+	  printf("Nu s-a gasit comoara cu id-ul %s in vanatoarea %s.\n",argv[3], argv[2]);
+	}
+      else
+	{
+	  printf("Comoara a fost stearsa din fisierul %s.\n",  entry->d_name);
+	}
+    }
+  //remove_hunt <hunt_id>
+  //pasi:verific daca exista (daca nu exista ma opresc cu un perror si un exit); sterg directorul cu tot ce mai contine folosind rm -r nume_director
+  //sa intrebi la lab ce faci cu logged_hunt daca tu stergi directorul in care se afla si logged_hunt!!!
+  //eu cred ca nu vei mai avea nevoie de fisierul text cu toate operatiile si deci se va sterge si el cu restul fisierelor din director si doar afisezi pe ecran ca s-a sters directorul
   return 0;
 }
