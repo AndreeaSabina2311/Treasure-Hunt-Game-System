efa7a5ceb57347e18fc70dc366ce549e498eb725
AndreeaSabina2311 <andreeasabina2311@gmail.com>
2025-05-15 22:30:38 +0300

    Add files via upload

--- proiect/phase3/treasure_hub.c
+++ proiect/phase3/treasure_hub.c
@@ -11,96 +11,94 @@
 pid_t monitor_pid = -1;
 int monitor_activ = 0;
 int monitor_terminating = 0;
+int pipefd[2]; //[0] citire; [1] scriere
 
 struct sigaction sa;
 struct dirent *entry;
+struct dirent *entry_local;
 
 //functii pentru semnale
+
+
 void handle_signal(int sig)
 {
   // folosit doar de monitor
-    if (sig == SIGUSR1) {
-        int fd = open("monitor_cmd.txt", O_RDONLY);
-        if (fd<0) {
-            perror("Monitor: nu pot deschide fișierul de comenzi");
-            return;
-        }
-
-       char cmd[256];
-        int n = read(fd, cmd, sizeof(cmd) - 1);
-        if (n < 0) {
-            perror("Monitor: eroare la citirea comenzii din fișier");
-            close(fd);
-            return;
-        }
-        cmd[n] = '\0';
-
-        // Elimină newline dacă există
-        char *newline = strchr(cmd, '\n');
-        if (newline) *newline = '\0';
-
-        close(fd);
-
-        if (strcmp(cmd, "list_hunts") == 0) {
-	  pid_t child = fork();
-	  if (child == 0) {
-	    execl("./treasure_manager", "./treasure_manager", "--list_hunts", NULL);
-	    perror("Monitor: exec eșuat pentru list_hunts");
-	    exit(1);
-	  } else if (child > 0) {
-	    waitpid(child, NULL, 0);
-	  } else {
-	    perror("Monitor: fork eșuat");
-	  }
-        } else if (strncmp(cmd, "--list_treasures ", 17) == 0) {
-            char *hunt_id = cmd + 17;
-	    while( *hunt_id==' ') hunt_id++;
-	    if(*hunt_id=='\0')
-	      {
-		printf("Monitor: lipseste id-ul vanatorului pentru list_treasures\n");
-	      }
-	    else{
-	      pid_t child = fork();
-	      if (child == 0) {
-		execl("./treasure_manager", "./treasure_manager", "--list_treasures", hunt_id, NULL);
-		perror("Monitor: exec eșuat pentru list_treasures");
-		exit(1);
-	      } else if (child > 0) {
-		waitpid(child, NULL, 0);
-	      } else {
-		perror("Monitor: fork eșuat");
-	      }
-	    }
-        } else if (strncmp(cmd, "--view_treasure ", 16) == 0) {
-	  char *args = cmd + 16;
-	  char args_copy[256];
-	    strncpy(args_copy, args, sizeof(args_copy)-1);
-	    args_copy[sizeof(args_copy)-1]='\0';
-	    
-            char *hunt = strtok(args_copy, " ");
-            char *treasure_id = strtok(NULL, " ");
-
-            if (hunt && treasure_id) {
-	      pid_t child = fork();
-	      if (child == 0) {
-		execl("./treasure_manager", "./treasure_manager", "--view_treasure", hunt, treasure_id, NULL);
-		perror("Monitor: exec eșuat pentru view_treasure");
-		exit(1);
-	      } else if (child > 0) {
-		waitpid(child, NULL, 0);
-	      } else {
-		perror("Monitor: fork eșuat");
-	      }
-            } else {
-	      perror("Monitor: argumente invalide pentru view_treasure");
-            }
-        } else {
-            perror("Monitor: comandă necunoscută");
-        }
-    } else if (sig == SIGTERM) {
-      printf("Monitorul se închide...\n");
-      usleep(1000000);
+  if (sig == SIGUSR1) {
+    int fd = open("monitor_cmd.txt", O_RDONLY);
+    if (fd<0) {
+      perror("Monitor: nu pot deschide fișierul de comenzi");
+      return;
     }
+
+    char cmd[256];
+    int n = read(fd, cmd, sizeof(cmd) - 1);
+    if (n <= 0) {
+      perror("Monitor: eroare la citirea comenzii din fișier");
+      close(fd);
+      return;
+    }
+    cmd[n] = '\0';
+
+    // Elimină newline dacă există
+    char *newline = strchr(cmd, '\n');
+    if (newline) *newline = '\0';
+
+    close(fd);
+
+    int child=fork();
+    if(child<0)
+      {
+	perror("Eroare creare proces copil pentru interpretare si executare comenzi");
+	exit(1);
+      }
+    if(child==0)
+      {
+        if (strcmp(cmd, "list_hunts") == 0) {
+	  execl("./treasure_manager", "./treasure_manager", "--list_hunts", NULL);
+	  perror("Monitor: exec eșuat pentru list_hunts");
+	  exit(1);
+        } else if (strncmp(cmd, "--list_treasures ", 17) == 0) {
+	  char *hunt_id = cmd + 17;
+	  while( *hunt_id==' ') hunt_id++;
+	  if(*hunt_id=='\0')
+	    {
+	      printf("Monitor: lipseste id-ul vanatorului pentru list_treasures\n");
+	      exit(1);
+	    }
+	    
+	  execl("./treasure_manager", "./treasure_manager", "--list_treasures", hunt_id, NULL);
+	  perror("Monitor: exec eșuat pentru list_treasures");
+	  exit(1);
+	      
+        } else if (strncmp(cmd, "--view_treasure ", 16) == 0){
+	  char *args=cmd+16;
+	  char *hunt=strtok(args," ");
+	  char *treasure_id=strtok(NULL," ");
+	  if(hunt && treasure_id)
+	    {
+	      execl("./treasure_manager","./treasure_manager","--view_treasure",hunt,treasure_id,NULL);
+	      perror("Monitor: exec failed");
+	      exit(1);
+	    }
+	  else
+	    {
+	      perror("Eroare: nu am primit hunt_id si treasure_id pentru --view_treasure");
+	      exit(1);
+	    }
+	}
+	else {
+	  perror("Monitor: comandă necunoscută");
+        }
+      } else if (child>0)
+      {
+	//proces parinte(monitor): asteapta copilul
+	waitpid(child, NULL, 0);
+      }
+  } else if (sig == SIGTERM) {
+    printf("Monitorul se închide...\n");
+    usleep(1000000);
+    exit(0);
+  }
 }
 
 
@@ -111,7 +109,7 @@ void setup_signal_handlers()
   sa.sa_flags=0;
 
   sigaction(SIGUSR1, &sa, NULL);
-  sigaction(SIGUSR2, &sa, NULL);
+  sigaction(SIGTERM, &sa, NULL);
 }
 
 //functii pentru procesul monitor
@@ -119,14 +117,16 @@ void setup_signal_handlers()
 void monitor_loop()
 {
   setup_signal_handlers();//activeaza functiile care trateaza semnalele
-  printf("Monitorul a pornit (PID: %d) și așteaptă comenzi prin semnale...\n", getpid());
-
+  dup2(pipefd[1], STDOUT_FILENO);
+  dup2(pipefd[1],STDERR_FILENO);
+  close(pipefd[0]);
+  close(pipefd[1]);
+  
   while(1)
     {
       pause(); //procesul asteapta pasiv sa primeasca un semnal
     }
-  printf("Monitorul iese acum\n");
-  exit(0);
+
 }
 
 void start_monitor()
@@ -137,16 +137,25 @@ void start_monitor()
       return;
     }
 
+  if(pipe(pipefd)<0)
+    {
+      perror("Eroare la crearea pipe-ului");
+      exit(1);
+    }
+  
   monitor_pid=fork();
   if(monitor_pid < 0)
     {
       perror("Eroare la crearea procesului monitor");
+      close(pipefd[0]);
+      close(pipefd[1]);
       return;
     }
   if(monitor_pid == 0)
     {
-      monitor_loop();
-      exit(0);
+      monitor_loop(); //copil
+       exit(0);
+      
     }
   monitor_activ=1;
   monitor_terminating=0;
@@ -174,9 +183,13 @@ void stop_monitor()
         printf("Monitorul s-a închis necontrolat.\n");
     }
 
+    close(pipefd[0]);
+    close(pipefd[1]);
+    
     monitor_pid = -1;
     monitor_terminating = 0;
 }
+
 
 void send_command_to_monitor(const char *cmd) {
     if (!monitor_activ) {
@@ -189,17 +202,25 @@ void send_command_to_monitor(const char *cmd) {
         perror("Eroare la deschiderea fișierului de comandă");
         return;
     }
-
-    if (write(fd, cmd, strlen(cmd)) < 0 || write(fd, "\n", 1) < 0) {
-        perror("Eroare la scrierea în fișierul de comandă");
-        close(fd);
-        return;
-    }
+    dprintf(fd,"%s\n",cmd);
     close(fd);
 
+    
     kill(monitor_pid, SIGUSR1);
-    usleep(100000);
+
+    usleep(1000000);
+
+    char buffer[2048];
+    int n=read(pipefd[0], buffer, sizeof(buffer)-1);
+    if(n>0)
+      {
+	buffer[n]='\0';
+	printf("%s\n", buffer);
+      }
+    else
+      printf("Monitorul nu a returnat nimic.\n");
 }
+
 
 
 ////Functie pentru faza 3
@@ -209,59 +230,60 @@ void calculate_scores()
   dir=opendir(".");
   if(!dir)
     {
-      perror("eroare deschidere director pentru functia calculate_scores");
+      perror("Eroare deschidere director pentru functia calculate_scores");
       exit(-1);
     }
 
-  while((entry=readdir(dir))!=NULL)
+  while((entry_local=readdir(dir))!=NULL)
     {
-      if(entry->d_type == DT_DIR && strncmp(entry->d_name,"game",4)==0)
+      if(entry_local->d_type == DT_DIR && entry_local->d_name[0]!='.')
 	{
-	  int pfd[2];
-	  if(pipe(pfd)<0)
+	  int pipe_local[2];
+	  if(pipe(pipe_local)==-1)
 	    {
-	      perror("Eroare creare pipe");
+	      perror("Eroare la creare pipe local");
 	      continue;
 	    }
-
-	  //creez procesul copil pentru functia de calcul a scorului
 	  int pid=fork();
 	  if(pid<0)
 	    {
-	      perror("Eroare la fork pentru creare copil");
+	      perror("Eroare fork calculate_scores");
 	      continue;
 	    }
-
-	  if(pid==0) //a fost creat procesul copil
+	  if(pid==0)
 	    {
-	      close(pfd[0]); //inchidem capatul de citire pentru a scrie in pipe
-	      dup2(pfd[1],STDOUT_FILENO);
-	      close(pfd[1]);
-	      execl("./calculate_score","calculate_score",entry->d_name,NULL);
-	      perror("execl");
-	      exit(-1);
+	      // Copil: scrie în pipe
+	      close(pipe_local[0]); // închide capătul de citire
+	      dup2(pipe_local[1], STDOUT_FILENO); // redirecționează stdout
+	      close(pipe_local[1]);
+
+	      execl("./calculate_score", "./calculate_score", entry_local->d_name, NULL);
+	      perror("Eroare la execl calculate_score");
+	      exit(1);
 	    }
 	  else
 	    {
-	      //proces parinte
-	      close(pfd[1]);
-	      printf("Scoruri pentru %s:\n",entry->d_name);
+	      // Părinte: citește din pipe
+	      close(pipe_local[1]); // închide capătul de scriere
 
-	      char buffer[256];
-	      int bytes;
-	      while((bytes=read(pfd[0], buffer, sizeof(buffer)-1))>0)
-		{
-		  buffer[bytes]='\0';
-		  printf("%s",buffer);
-		}
-	      close(pfd[0]);
-	      waitpid(pid,NULL,0);
+	      char buf[1024];
+	      int n;
+	      printf("Scoruri pentru %s:\n", entry_local->d_name);
+	      while ((n = read(pipe_local[0], buf, sizeof(buf) - 1)) > 0) {
+		buf[n] = '\0';
+		printf("%s", buf);
+	      }
+	      close(pipe_local[0]);
+	      waitpid(pid, NULL, 0); // așteaptă copilul curent
+	
 	    }
 	}
     }
 
-  closedir(dir);
+      closedir(dir);
+     
 }
+
 int main()
 {
 
@@ -279,6 +301,7 @@ int main()
       command[len]='\0';
       char *newline = strchr(command, '\n');
       if (newline) *newline = '\0';
+      
       if (monitor_terminating) {
 	printf("Monitorul este în curs de oprire. Așteptați finalizarea.\n");
 	continue;
@@ -291,13 +314,15 @@ int main()
             stop_monitor();
         }
         else if (strcmp(command, "--list_hunts") == 0) {
-            send_command_to_monitor("list_hunts");
+	  send_command_to_monitor("list_hunts");
+	  //char *args[] = {"./treasure_manager", "--list_hunts", NULL};
+	  //run_manager_command(args, "Listă de vânători");
         }
         else if (strncmp(command, "--list_treasures ", 17) == 0) {
-            send_command_to_monitor(command);
+	  send_command_to_monitor(command);
         }
         else if (strncmp(command, "--view_treasure ", 16) == 0) {
-            send_command_to_monitor(command);
+	  send_command_to_monitor(command);
         }
 	else if(strcmp(command, "--calculate_score")==0)
 	  {
